753539cadac0c9fd0e6786a1880a9cc77d21d01a
zahramaslavi <zahramaslavi@gmail.com>
2024-10-29 21:59:57 -0400

    added code deployment to aws

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
